2ab13a4211d1bd822476c95487c9f864f90099f6
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-11-11 18:53:09 +0900

    gulp less で特定テーマのlessがコンパイルされる

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var themeDirectories = ['./boilerplate/'];
+var lessPattern = '*.less';
+
+gulp.task('less', function() {
+    for (var directory of themeDirectories) {
+        gulp.src(directory + lessPattern)
+            .pipe(less({
+            }))
+            .pipe(gulp.dest(directory));
+    }
+});
 
 gulp.task('default', function() {
     // place code for your default task here
