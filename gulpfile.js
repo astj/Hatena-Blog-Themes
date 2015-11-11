59c04c7016995e27eff73bbb6d617cdffc5a8da2
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-11-11 19:09:11 +0900

    gulp で watch してくれる感じになった

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var themeDirectories = ['./boilerplate/'];
-var lessPattern = '*.less';
+var themeDirectoryNames = ['boilerplate', 'dummy']; // 増えたら増やす。1個だとなんかうまく動かないのでdummy入れておく。
+var lessPattern = '/*.less';
+var watchPattern = '/**/*.less'; // less/以下の変更もwatchする
+
+var generateWatchRegexp = function() {
+    return '{' + themeDirectoryNames.join() + '}' + watchPattern;
+};
 
 gulp.task('less', function() {
-    for (var directory of themeDirectories) {
+    for (var directory of themeDirectoryNames) {
         gulp.src(directory + lessPattern)
             .pipe(less({
             }))
@@ -13,5 +18,5 @@ gulp.task('less', function() {
 });
 
 gulp.task('default', function() {
-    // place code for your default task here
+    gulp.watch(generateWatchRegexp(), ['less']);
 });
