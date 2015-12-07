68651eec9bf9f6c2857b8f75d2614648ce26f69b
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-12-08 03:20:21 +0900

    手元のdropboxに置くかっこいいタスク

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var changed = require('gulp-changed');
 var themeDirectoryNames = ['boilerplate', 'mishiro']; // 要素1個だとうまく動かない気がする
 var lessPattern = '/*.less';
 var watchPattern = '/**/*.less'; // less/以下の変更もwatchする
+var dropboxPattern = '/*.css'; // 今の所配置するのはcssだけ
+var dropboxDirectoryPath = process.env.HOME + '/Dropbox/webfiles';
 
 var directoryGlob = function() {
     return '{' + themeDirectoryNames.join() + '}';
@@ -18,7 +21,15 @@ gulp.task('less', function() {
         .pipe(gulp.dest('.'));
 });
 
+// dropbox にコピーして素早くブラウザ確認できるようにする
+gulp.task('deploy-dropbox', function() {
+    gulp.src(directoryGlob() + dropboxPattern)
+        .pipe(changed(dropboxDirectoryPath))
+        .pipe(gulp.dest(dropboxDirectoryPath));
+});
+
 // 一度lessしておく
 gulp.task('default', ['less'], function() {
     gulp.watch(directoryGlob() + watchPattern,  ['less']);
+    gulp.watch(directoryGlob() + dropboxPattern,  ['deploy-dropbox']);
 });
